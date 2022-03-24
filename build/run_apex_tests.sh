49cb6701c3a85029980497d5649329b120141ee9
Yogesh Bhosale Vyom Trainers <93213458+vyomtrainers@users.noreply.github.com>
2022-03-24 23:00:15 +0530

    Update run_apex_tests.sh

--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -4,18 +4,18 @@
 
 #Create a scratch org
 #echo "Creating the Scratch Org..."
-#sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
+#sfdx force:org:create -f "Salesforce Template Application/config/project-scratch-def.json" -a ${CIRCLE_BRANCH} -s
 
 #sfdx force:source:push -u ${CIRCLE_BRANCH}
 
 #echo "Coverting source to metadata format"
-#sfdx force:source:convert -d test_code -r force-app
+#sfdx force:source:convert -d test_code -r "Salesforce Template Application/force-app"
 
 #echo "Deploying code to org"
 #sfdx force:mdapi:deploy --checkonly -u DevHub -d test_code/ -w -1 -l RunLocalTests
 
 echo "Deploying source to org"
-sfdx force:source:deploy --sourcepath force-app --targetusername DevHub
+sfdx force:source:deploy --sourcepath "Salesforce Template Application/force-app" --targetusername DevHub
 
 echo "Testing code in org"
 sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername DevHub
